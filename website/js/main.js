02fc96a21651fb6271ecbf917302199d1c31e054
Asheq Imran <asheq.imran@ge.com>
2019-03-02 13:33:26 -0800

    Update

--- website/js/main.js
+++ website/js/main.js
@@ -1,29 +1,37 @@
 const $ = jQuery
 
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  target: 'map',
+  view: new ol.View({
+    center: [-13585374.543476525694132, 4563668.506573964841664],
+    zoom: 13
+  })
+});
+
+
 $.getJSON('./data/neighborhoods_OpenLayers_CRS3857.geojson', function(geojsonObject) {
-  render(geojsonObject)
+  addLayer(geojsonObject, 'EPSG:3857')
 })
 
-function render(geojsonObject) {
+$.getJSON('./data/BlockGroups_Census_Simple.json', function(geojsonObject) {
+  addLayer(geojsonObject, 'EPSG::2227')
+})
+
+function addLayer(geojsonObject, projection) {
+  console.log(projection)
   var vectorSource = new ol.source.Vector({
-    features: (new ol.format.GeoJSON()).readFeatures(geojsonObject)
+    features: (new ol.format.GeoJSON()).readFeatures(geojsonObject),
+    projection: projection
   });
 
   var vectorLayer = new ol.layer.Vector({
-    source: vectorSource,
+    source: vectorSource
   });
 
-  var map = new ol.Map({
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      }),
-      vectorLayer
-    ],
-    target: 'map',
-    view: new ol.View({
-      center: [-13585374.543476525694132, 4563668.506573964841664],
-      zoom: 13
-    })
-  });
+  map.addLayer(vectorLayer)
 }
